c8888f3eddbb1be663e180f659d4c289c0ec1ee9
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-07-12 16:31:10 +0000

    repo init

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+var getJSON = function(url) {
+	return new Promise(function(resolve, reject){
+		var ajax = new XMLHttpRequest();
+		ajax.open('GET', url);
+		ajax.send();
+		ajax.onreadystatechange = function() {
+			if(ajax.readyState == 4){
+				var response = JSON.parse(ajax.responseText);
+				resolve(response);
+			}
+		}
+	});
+}
+
+getJSON('data/earth-like-results.json')
+	.then(function(mensaje){return getJSON(mensaje.results[0])})
+	.then(function(resultado){console.log(resultado)})
---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+app.use('/data', express.static(__dirname + '/data'));
+app.use('/static', express.static(__dirname + '/assets'));
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+	console.log("escuchando servidor en puerto 8000");
+});
+
+app.listen(8000);
