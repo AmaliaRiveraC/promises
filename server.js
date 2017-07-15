2646fcf0f9654564989efd98373f9637fea7018e
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-07-15 15:53:28 -0500

    impreson de tarjetas sin fetch

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-var express = require('express');
 
+
+var express = require("express");
+var path = require("path");
 var app = express();
 
-app.use('/data', express.static(__dirname + '/data'));
-app.use('/static', express.static(__dirname + '/assets'));
-
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
-	console.log("escuchando servidor en puerto 8000");
+app.use('/data', express.static(path.join(__dirname,'data')));
+app.use('/static', express.static(path.join(__dirname,'assets')));
+app.use('/static', express.static(path.join(__dirname,'node_modules')));
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+  console.log("escuchando servidor 1234")
 });
 
-app.listen(8000);
+app.listen(1234);
